9a4ec21cbc4a88aeaaef2fb0afa97672604ac6c5
박겸영 <yong_drg@naver.com>
2023-03-09 00:38:03 +0900

    add web font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import { Chat } from '../components/Chat';
-import { Input } from '../components/Input';
+import { Chat } from "../components/Chat";
+import { Input } from "../components/Input";
 
 function Home() {
   return (
     <div className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <h1>맛집 리뷰 생성기</h1>
+        <h1 className="font-title">쩝쩝박사</h1>
+        <h2>맛집 리뷰 생성기</h2>
         <div className="text-zinc-600">
-          a simple chat bot is implemented using Next.js, API Routes, and OpenAI API.
+          a simple chat bot is implemented using Next.js, API Routes, and OpenAI
+          API.
         </div>
       </section>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('@tailwindcss/forms')],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [require("@tailwindcss/forms")],
+  theme: {
+    colors: {
+      tomato: "#e56353",
+    },
+
+    fontFamily: {
+      title: "PyeongChangPeace-Bold",
+    },
+  },
 };
