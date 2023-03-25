fcc568a47c5243e5317d6221bfb1cb74bed5c8f8
박겸영 <yong_drg@naver.com>
2023-03-26 04:54:28 +0900

    feat: add tone

---
+++ constants/tone.ts
@@ -0,0 +1,20 @@
+const TONE = [
+  {
+    id: 0,
+    name: "~했어요.",
+  },
+  {
+    id: 1,
+    name: "~했습니다.",
+  },
+  {
+    id: 2,
+    name: "~했어.",
+  },
+  {
+    id: 3,
+    name: "~했다.",
+  },
+];
+
+export default TONE;
