4d564af0113c2b7077f84fb7816bbca98061330d
박겸영 <yong_drg@naver.com>
2023-04-12 14:31:11 +0900

    refactor: GA Head 태그 내로 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,6 +58,23 @@ function App({ Component, pageProps }: AppProps) {
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
 
+        {/* Google tag (gtag.js) - Google Analytics */}
+        <Script
+          async
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         {/* Google Adsense */}
         <Script
           async
@@ -65,23 +82,6 @@ function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
-
-      {/* Google tag (gtag.js) - Google Analytics */}
-      <Script
-        async
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gtag.GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
 
       <Layout>
         <Component {...pageProps} />
